17d39c68c3d7f8f941b1409ea98c416a02d8a3b4
Lori Holden <email@loriholden.com>
2010-10-13 07:55:01 -0600

    Added emacs

---
+++ emacs/dot_emacs.d/custom.el
@@ -0,0 +1,24 @@
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(auto-save-default nil)
+ '(default-frame-alist (quote ((tool-bar-lines . 0) (menu-bar-lines . 1) (background-color . "#000000") (foreground-color . "#FFFFFF") (cursor-color . "#DAD085") (width . 120) (height . 60))))
+ '(eshell-scroll-to-bottom-on-output (quote all))
+ '(indent-tabs-mode nil)
+ '(inhibit-startup-screen t)
+ '(make-backup-files nil)
+ '(mouse-wheel-scroll-amount (quote (1)))
+ '(normal-erase-is-backspace t)
+ '(scroll-conservatively 1)
+ '(scroll-preserve-screen-position 1)
+ '(show-paren-mode t)
+ '(standard-indent 2)
+ '(tab-width 2))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(show-paren-match ((((class color) (background dark)) (:background "steelblue4")))))
